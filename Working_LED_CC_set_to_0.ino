2ac71e65a57bf096303ccead9deb594730eead90
Staf Smith <staffordsmith83@gmail.com>
2020-08-19 17:48:52 +0800

    Formatting and comments on both versions. Added attribution link.

--- Working_LED_CC_set_to_0.ino
+++ Working_LED_CC_set_to_0.ino
@@ -1,37 +1,56 @@
+// Arduino code.
+// Takes input from a physical switch input on the Arduino board (pin2)
+// and translates it into a MIDI Control Change message that is sent over a MIDI cable to another device.
+// Inspired by https://dikant.de/2015/01/18/diy-midi-footswitch-controller/
 
+
+// initialise the switchState
 int switchState = 0;
+
+// Define the message you want to send
+int midiChannel = 1;
+int midiCommand = 102;
+int midiValue1 = 0;
+int midiValue2 = 127;
+// The provided values will send:
+// MIDI CC 102 on channel 1 with value 0 or 127 to turn OFF or ON the
+// Global Bypass function on a Strymon Timeline effect device.
+
+// setup method
 void setup() {
-  pinMode(12,OUTPUT);
-  pinMode(2,INPUT); 
-  // Setup serial port for MIDI communication
-  Serial.begin(31250); 
+    pinMode(12,OUTPUT);
+    pinMode(2,INPUT);
+    // Setup serial port for MIDI communication
+    Serial.begin(31250);
 }
+
+// the loop method is the main method for Arduino. 
+// Will run when power is provided to the microcontroller.
 void loop() {
-switchState = digitalRead(2);
-//this reads from pin 2
+    switchState = digitalRead(2);
+    //this reads from pin 2
 
-if (switchState == LOW) {
-// the button is not pressed
+    if (switchState == LOW) {
+    // the switch is not pressed
+    digitalWrite(12, HIGH);
+    // message that will be sent when switch is released
+    midiCc(midiChannel, midiCommand, midiValue1);
+    }
 
-  digitalWrite(12, HIGH);
-  // send CC 102 on channel 1 with value 0 to turn OFF Global Bypass on Strymon Timeline
-      midiCc(1, 102, 0);    
-  }
-else {
-
-  // the button is pressed
-  
-  digitalWrite(12, LOW);
-  // send CC 102 on channel 1 with value 0 to turn OFF Global Bypass on Strymon Timeline
-      midiCc(1, 102, 127);
-  delay(50);
-  }
+    else {
+    // the switch is pressed
+    digitalWrite(12, LOW);
+    // message that will be sent when switch is triggered
+    midiCc(midiChannel, midiCommand, midiValue2);
+    delay(50);
+    }
 } 
 
+// this method called by the loop() method
 void midiCc(int channel, int command, int value) {
-  Serial.write(175 + channel);
-  Serial.write(command);
-  Serial.write(value);
-  }
+    Serial.write(175 + channel);
+    Serial.write(command);
+    Serial.write(value);
+}
  
 
--- Working_Model.ino
+++ Working_Model.ino
@@ -1,59 +1,74 @@
+// Arduino code.
+// Takes input from a physical switch input on the Arduino board (pin2)
+// and translates it into a MIDI Control Change message that is sent over a MIDI cable to another device.
+// THIS VERSION: incorporates a check for a change in state of the switch. Looks for state changes
+// so that the message is only sent if the pedal is pressed or released.
+// Inspired by https://dikant.de/2015/01/18/diy-midi-footswitch-controller/
+
+
+// Set Constants:
 // pin to use for indicator LED
 int ledPin = 12;
 // pin that is connected to the footswitch
-int buttonPin = 2;
- 
-int buttonState = 0;
-int buttonLastState = 0;
-int buttonInitState = 0;
- 
-int muteStatus = 0;
- 
+int switchPin = 2;
+
+// initialise states:
+int switchState = 0;
+int switchLastState = 0;
+int switchInitState = 0;
+int messageStatus = 0;
+
+// setup method
 void setup() {
-  pinMode(ledPin, OUTPUT);
-  pinMode(buttonPin, INPUT_PULLUP);
-  
-  // Setup serial port for MIDI communication
-  Serial.begin(31250);
-  
-  // read current button state, a press is detected whenever
-  // state changes to anything else than the init state
-  buttonInitState = digitalRead(buttonPin);
-  // we only want to do something on state changes
-  buttonLastState = buttonInitState;
+    pinMode(ledPin, OUTPUT);
+    pinMode(switchPin, INPUT_PULLUP);
+
+    // Setup serial port for MIDI communication
+    Serial.begin(31250);
+
+    // Is the switch pressed when we first power up the board? Read the pin
+    switchInitState = digitalRead(switchPin);
+    // Set the switchLastState to be this initial state
+    switchLastState = switchInitState;
 }
- 
+
+// the loop method is the main method for Arduino. 
+// Will run when power is provided to the microcontroller.
 void loop() {
-  buttonState = digitalRead(buttonPin);
-  
-  // has the state changed?
-  // if buttonState NOT EQUAL to buttonLastState
-  if (buttonState != buttonLastState) { 
-    buttonLastState = buttonState;
-    if (buttonState = buttonInitState) {
-      // button is in pressed state, trigger action
-      if (muteStatus == 0) {
-        // mute and light up LED
-        digitalWrite(ledPin, HIGH);
-        // send CC 85 on channel 2 with value 0 to mute
-        // mute group 6 on a Behringer X32
-        midiCc(1, 102, 127);
-        muteStatus = 1;
-      } else {
-        // unmute and turn off LED
-        digitalWrite(ledPin, LOW);
-        midiCc(1, 102, 0);
-        muteStatus = 0;
-      }
-    }
-    // workaround to prevent fluttering of button state
+    // read the switch state again to see if it has changed
+    switchState = digitalRead(switchPin);
+
+    // has the state changed?
+    // if switchState NOT EQUAL to switchLastState, then run the code
+    if (switchState != switchLastState) {
+        switchLastState = switchState;
+        if (switchState = switchInitState) {
+        // if switch is in pressed state, send the pressed message
+            if (messageStatus == 0) {
+                // send on message and light up LED
+                digitalWrite(ledPin, HIGH);
+                // Send MIDI CC 102 on channel 1 with value 127
+                midiCc(1, 102, 127);
+                messageStatus = 1;
+            } else {
+                // send off message and turn off LED
+                digitalWrite(ledPin, LOW);
+                // Send MIDI CC 102 on channel 1 with value 127
+                midiCc(1, 102, 0);
+                messageStatus = 0;
+            }
+        }
+    // delay for 50 milliseconds before checking for state change again
+    // to deal with electrical switch 'bounce'
     delay(10);
-  }
+
+  } // then go back to the top of the loop and check for state change again
 }
- 
+
+// this method called when we want to send a message
 void midiCc(int channel, int command, int value) {
-  Serial.write(175 + channel);
-  Serial.write(command);
-  Serial.write(value);
+    Serial.write(175 + channel);
+    Serial.write(command);
+    Serial.write(value);
 }
 
